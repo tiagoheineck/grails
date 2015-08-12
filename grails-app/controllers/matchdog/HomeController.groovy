dec554ff1970694029fef9aa71d40f8be0f0dd14
Artur Alves <arturcalves@gmail.com>
2015-08-12 20:46:06 -0300

    #10. agora sim o carrossel respeita a busca

--- grails-app/controllers/matchdog/HomeController.groovy
+++ grails-app/controllers/matchdog/HomeController.groovy
@@ -2,8 +2,44 @@ package matchdog
 
 class HomeController {
 
+    def sessionFactory
+
     def index(){
-    	[alvos : Dog.find('from Dog where dono_id != :dono_id', [dono_id: session['dono_id']]) ]
+
+        def cSession = sessionFactory.currentSession 
+
+        String query = $/
+            select
+            d.*
+            from DOG d
+            inner join DONO dn on dn.id = d.dono_id
+            left outer join CURTIDA c on (c.dog_alvo_id = d.id and c.dog_id = :dogAtual_id)
+            where dn.id <> :dogAtual_dono_id
+            and c.id is null
+            and dn.sexo = :faro_sexoDono
+            and d.sexo =  :faro_sexoDog
+            and d.raca_id =  :faro_raca
+            and d.cidade_id =  :faro_cidade
+            and (d.interessa_passear = :faro_interessaPassear
+                    or
+                    d.interessa_cruzar = :faro_interessaCruzar)
+        /$
+
+        def alvos = cSession
+                .createSQLQuery(query)
+                        .addEntity(matchdog.Dog)
+                        .setLong('dogAtual_dono_id', session['dono_id'])
+                        .setLong('dogAtual_id', session['dog_id'])
+                        .setString('faro_sexoDono', session['current_faro'].sexoDono)
+                        .setString('faro_sexoDog', session['current_faro'].sexoDog)
+                        .setLong('faro_raca', session['current_faro'].raca.id)
+                        .setLong('faro_cidade', session['current_faro'].cidade.id)
+                        .setBoolean('faro_interessaPassear', session['current_faro'].interessaPassear )
+                        .setBoolean('faro_interessaCruzar', session['current_faro'].interessaCruzar )
+                        .list()
+         
+        [alvos : alvos ]
+    	//[alvos : Dog.find('from Dog where dono_id != :dono_id', [dono_id: session['dono_id']]) ]
     }
 
     def farejar(){
