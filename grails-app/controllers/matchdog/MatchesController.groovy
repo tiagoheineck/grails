f4eda512523c6c9ceffae6e36e11ae464045916f
Artur Alves <arturcalves@gmail.com>
2015-08-12 14:33:42 -0300

    marcar matches como vistos, e nao rola mais ok

--- grails-app/controllers/matchdog/MatchesController.groovy
+++ grails-app/controllers/matchdog/MatchesController.groovy
@@ -4,23 +4,21 @@ class MatchesController {
 
     def index() {
         
-        def matches = [:]
-
-        [matches : Match.find('from Match where dog1_id = :dog_id OR dog2_id = :dog_id AND (DATAHORA_DOG1DESISTIU is null and DATAHORA_DOG2DESISTIU is null)  )', [dog_id: session['dog_id']])]
+        def matches =  Match.find('from Match where dog1_id = :dog_id OR dog2_id = :dog_id AND (DATAHORA_DOG1DESISTIU is null and DATAHORA_DOG2DESISTIU is null)  )', [dog_id: session['dog_id']])
         
-    	/* ARTUR, ESSE TRECHO AQUI QUE NÃO FUNCIONA
         matches.each {
     		switch(session['dog_id']) {
     			case it.dog1.id:
-    				m.datahoraDog1Viu = Calendar.instance.time
+    				it.datahoraDog1Viu = Calendar.instance.time
     				break
     			case it.dog2.id:
-    				m.datahoraDog2Viu = Calendar.instance.time
+    				it.datahoraDog2Viu = Calendar.instance.time
     				break
     		}
     		it.save(flush:true)
     	}
-        */
+
+        [matches: matches]
 
     }
 
@@ -28,10 +26,10 @@ class MatchesController {
     	def m = Match.get(params.id)
     	switch(session['dog_id']) {
     		case m.dog1.id:
-    			m.datahora_dog1_desistiu = Calendar.instance.time
+    			m.datahoraDog1Desistiu = Calendar.instance.time
     			break
     		case m.dog2.id:
-    			m.datahora_dog2_desistiu = Calendar.instance.time
+    			m.datahoraDog2Desistiu = Calendar.instance.time
     	}
     	m.save(flush:true)
     	redirect(controller: "matches")
